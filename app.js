0edc7f4528f92349861103b4e1abd0bc38968051
Lucas Vinicius <l.vinicius1577@gmail.com>
2023-02-22 17:05:54 -0300

    sexoAluno e sexoProf nao finalizado, mas de resto tudo feito

--- app.js
+++ app.js
@@ -15,89 +15,109 @@ var entradaDados = readline.createInterface({
     output: process.stdout
 })
 
-entradaDados.question('Qual o nome do(a) aluno(a)?: \n', function(nomeAluno){
+entradaDados.question('Qual o nome do(a) aluno(a)?: \n', function (nomeAluno) {
     let estudante = nomeAluno
 
-    entradaDados.question('Sexo do(a) aluno(a)? [masculino, feminino]: ', function(sexoAluno){
+    entradaDados.question('Sexo do(a) aluno(a)? [masculino, feminino]: ', function (sexoAluno) {
         let estudanteSexo = sexoAluno
-        const sexoEstudante = mediaEscolar.validarSexoAluno(estudanteSexo)
+        const sexoEstudante = mediaEscolar.validarSexoAluno(sexoAluno)
 
-        entradaDados.question('Qual o nome do(a) professor(a)?: \n', function(nomeProf){
+        entradaDados.question('Qual o nome do(a) professor(a)?: \n', function (nomeProf) {
             let mentor = nomeProf
 
-            entradaDados.question('Sexo do(a) professor(a)?: [masculino, feminino]: ', function(sexoProf){
-                let mentorSexo = sexoProf.toLowerCase()
-                const sexoMentor = mediaEscolar.validarSexoProf(mentorSexo)
-                
-                entradaDados.question('Qual o nome do curso?: \n', function(nomeCurso){
+            entradaDados.question('Sexo do(a) professor(a)?: [masculino, feminino]: ', function (sexoProf) {
+                let mentorSexo = sexoProf
+                const sexoMentor = mediaEscolar.validarSexoProf(sexoProf)
+
+                entradaDados.question('Qual o nome do curso?: \n', function (nomeCurso) {
                     let curso = nomeCurso
 
-                    entradaDados.question('Qual o nome da disciplina?: \n', function(nomeDisciplina){
+                    entradaDados.question('Qual o nome da disciplina?: \n', function (nomeDisciplina) {
                         let disciplina = nomeDisciplina
                         console.log('------------------------------------------------------')
 
-                        entradaDados.question('Digite a primeira nota: ', function(nota1){
-                           let primeiraNota = Number(nota1.replace(',', '.'))
+                        entradaDados.question('Digite a primeira nota: ', function (nota1) {
+                            let primeiraNota = Number(nota1.replace(',', '.'))
 
-                            entradaDados.question('Digite a segunda nota: ', function(nota2){
+                            entradaDados.question('Digite a segunda nota: ', function (nota2) {
                                 let segundaNota = Number(nota2.replace(',', '.'))
 
-                                entradaDados.question('Digite a terceira nota: ', function(nota3){
+                                entradaDados.question('Digite a terceira nota: ', function (nota3) {
                                     let terceiraNota = Number(nota3.replace(',', '.'))
 
-                                    entradaDados.question('Digite a quarta nota: ', function(nota4){
+                                    entradaDados.question('Digite a quarta nota: ', function (nota4) {
                                         let quartaNota = Number(nota4.replace(',', '.'))
 
-                                        const fechamentoApp = entradaDados.close()
+                                        const validarErrosNotas = mediaEscolar.validarNotas(primeiraNota, segundaNota, terceiraNota, quartaNota)
+                                        const campoVazio = mediaEscolar.campoVazioStrings(estudante, estudanteSexo, mentor, mentorSexo, curso, disciplina)
 
-                                        const validarCampos = mediaEscolar.campoVazioStrings(
-                                            nomeAluno,
-                                            sexoAluno, 
-                                            nomeProf, 
-                                            sexoProf, 
-                                            nomeCurso, 
-                                            nomeDisciplina
-                                            )
+                                        if (campoVazio)
+                                            entradaDados.close()
+                                        else if(validarErrosNotas)
+                                            entradaDados.close
+                                        else if (nota1 <= 100 && nota1 >= 0 || nota2 <= 100 && nota2 >= 0 || nota3 >= 100 && nota3 <= 0 || nota4 >= 100 && nota4 <= 0) {
 
-                                        const validarECalcNotas = mediaEscolar.validarNotasAluno(
-                                            primeiraNota,
-                                            segundaNota,
-                                            terceiraNota,
-                                            quartaNota
-                                            )
+                                            let alunoNotaFinal = mediaEscolar.validarAlunoNota(primeiraNota, segundaNota, terceiraNota, quartaNota)
 
-                                        const alunoMedia = mediaEscolar.alunoAvaliacao(validarECalcNotas)
-
-                                        const imprimirNaTela = mediaEscolar.mostraNaTela(
-                                            nomeAluno,
-                                            sexoEstudante,
-                                            nomeProf, 
-                                            sexoMentor,
-                                            nomeCurso,
-                                            nomeDisciplina,
-                                            primeiraNota,
-                                            segundaNota,
-                                            terceiraNota, quartaNota
-                                            )
-
-                                            console.log(imprimirNaTela)
-
-                                            if(alunoMedia === `    Em exame com a média: ${validarECalcNotas}     `){
-                                                console.log(alunoMedia)
-                                                entradaDados.question('Aluno(a) precisou fazer a prova de exame, nota do exame?: ',
-                                                 function (notaExame){
-                                                    let exame = Number(notaExame)
-                                                    let somaExame = exame + validarECalcNotas
-                                                    let mediaExameResultado = somaExame /2
-                                                    console.log(mediaEscolar.alunoExame(mediaExameResultado, validarECalcNotas))
-                                                })
+                                            if (alunoNotaFinal >= 70) {
+                                            console.log(`
+                                            ${estudanteSexo}: ${estudante}
+                                            ${mentorSexo}: ${mentor} 
+                                            Curso: ${curso}
+                                            Disciplina: ${disciplina}
+                                            Notas: primeira nota: ${primeiraNota}| segunda nota: ${segundaNota}| terceira nota: ${terceiraNota}| quarta nota: ${quartaNota}
+                                            Situação: ${alunoNotaFinal}
+                                                            PASSOU!
+                                            `)
+                                            entradaDados.close()
                                             }
+                                            else if (alunoNotaFinal >= 50 && alunoNotaFinal <= 69) {
+                                                entradaDados.question("Aluno(a) em exame, nota do exame?: ", function (notaExame) {
 
-                                                       
-                                            
-                                                                        
+                                                    let exame = notaExame;
+                                                    let resultadoExame = 0
+                                                    resultadoExame = mediaEscolar.validarAlunoExame(alunoNotaFinal, exame)
+                                                    if (resultadoExame >= 60) {
+                                                        console.log(`
+                                                    ${estudanteSexo}: ${estudante}
+                                                    ${mentorSexo}: ${mentor} 
+                                                    Curso: ${curso}
+                                                    Disciplina: ${disciplina}
+                                                    Notas: primeira nota: ${primeiraNota}| segunda nota: ${segundaNota}| terceira nota: ${terceiraNota}| quarta nota: ${quartaNota}
+                                                    Situação: ${resultadoExame}
+                                                                PASSOU!
+                                                    `)
+                                                    entradaDados.close()
+                                                    } else {
+                                                        console.log(`
+                                                    ${estudanteSexo}: ${estudante}
+                                                    ${mentorSexo}: ${mentor} 
+                                                    Curso: ${curso}
+                                                    Disciplina: ${disciplina}
+                                                    Notas: primeira nota: ${primeiraNota}| segunda nota: ${segundaNota}| terceira nota: ${terceiraNota}| quarta nota: ${quartaNota}
+                                                    Situação: ${resultadoExame}
+                                                                REPROVOU!
+                                                    `)
+                                                    entradaDados.close()
+                                                    }
+                                                })
+                                            } else {
+                                                console.log(`
+                                                ${sexoAluno}: ${estudante}
+                                                ${sexoProf}: ${mentor} 
+                                                Curso: ${curso}
+                                                Disciplina: ${disciplina}
+                                                Notas: primeira nota: ${primeiraNota}| segunda nota: ${segundaNota}| terceira nota: ${terceiraNota}| quarta nota: ${quartaNota}
+                                                Situação: ${alunoNotaFinal}
+                                                            REPROVOU!
+                                                `)
+                                                entradaDados.close()
+                                            }
+                                        }
+                                        else
+                                        console.log('INSIRA NÚMEROS ENTRE 0 A 100')
                                     })
-                                })   
+                                })
                             })
                         })
                     })
@@ -106,6 +126,18 @@ entradaDados.question('Qual o nome do(a) aluno(a)?: \n', function(nomeAluno){
         })
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -1,9 +1,18 @@
-/********
- *  Objetivo: Arquivo de funções para realizar funçoes 
- * Data: 13/02/2023
- * Versão: 1.1
- */
-const campoVazioStrings = function (nomeAluno, sexoAluno, nomeProf, sexoProf, nomeCurso, nomeDisciplina) {
+const validarNotas = function(nota1, nota2, nota3, nota4){
+    let valor1 = Number(nota1)
+    let valor2 = Number(nota2)
+    let valor3 = Number(nota3)
+    let valor4 = Number(nota4)
+
+    if (isNaN(valor1) || isNaN(valor2) || isNaN(valor3) || isNaN(valor4))
+        console.log('ERROR: INSIRA APENAS NÚMEROS!')
+    else if (valor1 == '' || valor2 == '' || valor3 == '' || valor4 == '')
+        console.log('ERROR: PREENCHA OS CAMPOS')
+
+        
+}
+
+const campoVazioStrings = function(nomeAluno, sexoAluno, nomeProf, sexoProf, nomeCurso, nomeDisciplina){
     let nomeDoALuno = nomeAluno
     let sexoDoALuno = sexoAluno
     let nomeDoProf = nomeProf
@@ -31,102 +40,56 @@ const campoVazioStrings = function (nomeAluno, sexoAluno, nomeProf, sexoProf, no
             console.log('ERRO: Apenas letras, nos primeiros campos')
 }
 
+function validarAlunoNota(nota1, nota2, nota3, nota4) {
 
-
-const validarNotasAluno = function (nota1, nota2, nota3, nota4){
     let valor1 = Number(nota1)
     let valor2 = Number(nota2)
     let valor3 = Number(nota3)
     let valor4 = Number(nota4)
-    
+    let media
 
-    if(
-        valor1 == '' ||
-        valor2 == '' ||
-        valor3 == '' ||
-        valor4 == ''
-    ){
-        console.log('ERRO: preencha os campos vazios')
-        
-    }
-        
-    else if (
-        isNaN(valor1) || 
-        isNaN(valor2) || 
-        isNaN(valor3) ||
-        isNaN(valor4)
-    )
-        console.log('ERRO: Apenas números, nos campos nota')
-    else if (
-        valor1 <0 || valor1 >100 ||
-        valor2 <0 || valor2 >100 ||
-        valor3 <0 || valor3 >100 ||
-        valor4 <0 || valor4 >100
-    )
-        console.log('ERRO: Apenas notas entre 0 a 100')   
-    else{
-        let contaMedia = (valor1 + valor2 + valor3 + valor4) 
-        let mediaResultado = contaMedia / 4
-        return mediaResultado.toFixed(1)
-    }
-}
-const alunoAvaliacao = function(validarNotas){
-    if(validarNotas >=70)
-        return `    Aprovado(a) com a média: ${validarNotas}  `
-    else if(validarNotas >=50 && validarNotas <=69)
-        return `    Em exame com a média: ${validarNotas}     `
-    else if(validarNotas <50)
-        return `    Reprovado(a) com a média: ${validarNotas} `
-}
 
-const alunoExame = function(notaExameResultado, validarNotas){
-    if(notaExameResultado < 50 )
-       console.log(`Reprovado no exame, média final: ${notaExameResultado}`) 
-    else if(notaExameResultado >= 50)
-        console.log(`Aprovado no exame, média final: ${notaExameResultado}`) 
+    media = (Number(valor1) + Number(valor2) + Number(valor3) + Number(valor4)) / 4
+
+    if (media >= 70)
+        return media
+
     else
-        return false 
+        return media
+
 }
 
-const validarSexoAluno = function(sexoALuno) {
+const validarAlunoExame = (mediaNormal, exame) =>
+    (Number(mediaNormal) + Number(exame)) / 2;
+
+
+const validarSexoProf = function(sexoProf){
     
-    if(sexoALuno === 'masculino')
-        return 'Aluno'
-    else if(sexoALuno === 'feminino')
-        return 'Aluna'
-    else    
-        return null
-}
-
-const validarSexoProf = function (sexoProf) {
     if(sexoProf === 'masculino')
         return 'Professor'
     else if(sexoProf === 'feminino')
         return 'Professora'
     else    
-        return null
+        return false
 }
 
-
-const mostraNaTela = function (
-    nomeAluno, sexoAluno, nomeProf, sexoProf, nomeCurso, nomeDisciplina, nota1, nota2, nota3, nota4, alunoMedia
-    ) {
-    return `
-    ${sexoAluno}: ${nomeAluno}
-    ${sexoProf}: ${nomeProf} 
-    Curso: ${nomeCurso}
-    Disciplina: ${nomeDisciplina}
-    Notas: primeira nota: ${nota1}| segunda nota: ${nota2}| terceira nota: ${nota3}| quarta nota: ${nota4}
-    Situação: ${alunoMedia}
-     `
+const validarSexoAluno = function(sexoAluno){
+   
+    if(sexoAluno === 'masculino')
+        return 'ALuno'
+    else if(sexoAluno === 'feminino')
+        return 'Aluna'
+    else    
+        return false
+    
 }
+
 
 module.exports = {
-    campoVazioStrings,
-    validarNotasAluno,
-    validarSexoAluno,
     validarSexoProf,
-    alunoAvaliacao,
-    alunoExame,
-    mostraNaTela,
+    validarSexoAluno,
+    validarAlunoNota,
+    validarAlunoExame,
+    validarNotas,
+    campoVazioStrings
 }
